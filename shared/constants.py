d302bd264680e69c1ada4cf054c091b712251e62
SewerLabs1 <sewerlabs@proton.me>
2023-12-06 11:14:22 -0500

    Update constants.py

--- shared/constants.py
+++ shared/constants.py
@@ -1,6 +1,6 @@
 # Set to your app's URL
 OPENROUTER_REFERRER = "https://huggingface.co/spaces/nonhuman/neuraldex"
 OPENROUTER_BASE = "https://openrouter.ai"
-OPENROUTER_API_BASE = "https://openrouter.ai/api/v1/chat/completions"
+OPENROUTER_API_BASE = f"{OPENROUTER_BASE}/api/v1"
 OPENROUTER_DEFAULT_CHAT_MODEL = "gryphe/mythomist-7b"
 OPENROUTER_DEFAULT_INSTRUCT_MODEL = "gryphe/mythomist-7b"
